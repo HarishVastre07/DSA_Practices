19d1b0ef04e992d88cca0aa6bc91dfc25aea890f
Harish <harisvastre7@gmail.com>
2025-12-16 16:58:58 +0530

    low_freq code updated

--- low_freq.cpp
+++ low_freq.cpp
@@ -1,33 +1,37 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
+
 int mostFrequentElement(vector<int>& nums)
- {
+{
     int maxNum = 0;
-    for(auto n : nums)
-    {
+    for (int n : nums) {
         maxNum = max(maxNum, n);
     }
-    vector<int> freq(maxNum +1 , 0);
-       for(int i = 0; i<nums.size(); i++)
-       {
-        freq[nums[i]] += 1;
-       }
 
-       int max=0;
-          for(int i=0; i<freq.size(); i++)
-        {
-         if(freq[i]>max)
-         max = i;
-         }
-         
-         cout << "Most Frequent Element(s): "<< max ;
+    vector<int> freq(maxNum + 1, 0);
 
+    // Count frequency
+    for (int n : nums) {
+        freq[n]++;
     }
 
-    int main()
-    {
-        vector<int> nums = {1,2,2,3,3,3,3,3,3,4,};
-        mostFrequentElement(nums);
+    int maxFreq = 0;
+    int result = 0;
 
+    // Find highest frequency
+    for (int i = 0; i < freq.size(); i++) {
+        if (freq[i] > maxFreq) {
+            maxFreq = freq[i];
+            result = i;
+        }
     }
+
+    return result;
+}
+
+int main()
+{
+    vector<int> nums = {1,2,2,3,3,4,4};
+    cout << "Most Frequent Element: " << mostFrequentElement(nums);
+}

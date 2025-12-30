7a1fc8006dd784607871d53d0a60c9c1a2339489
Harish <harisvastre7@gmail.com>
2025-12-30 23:45:11 +0530

    Missing Number file added

--- missing_number.cpp
+++ missing_number.cpp
@@ -1,37 +1,30 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-void missing(vector<int>&nums, int n)
+int missing(vector<int>&nums, int n)
 {
-    // Create a boolean array to mark present numbers
-    vector<bool> present(n+1, false);
-    
-    // Mark all numbers that are present in the array
-    for(int num : nums)
+    for(int i=0;i<n;i++)
     {
-        if(num >= 0 && num <= n)
+        int flag =0;
+        for(int j=0;j<n-1;j++)
         {
-            present[num] = true;
+            if(nums[j]==i)
+            {
+                flag = 1;
+                break;
+            }
         }
+        if(flag==0)
+        return i;
     }
-    
-    // Find and print missing numbers from 0 to n
-    cout << "Missing number(s): ";
-    for(int i=0; i<=n; i++)
-    {
-        if(!present[i])
-        {
-            cout << i << " ";
-        }
-    }
-    cout << endl;
+    return -1;
 }
 
 int main()
 {
     int n=6;
     vector<int> nums={0,1,2,4,5,6};
-    
-    missing(nums, n);
+    int ans = missing(nums,n);
+    cout << "Missing Number is " << ans << endl;
     return 0;
 }
